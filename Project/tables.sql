15128b20bb1432cc08397b7e673836f5f3ff97d9
Unknown <connor@applicationx.net>
2019-03-28 21:25:15 -0400

    Updated add/drop order and added foreign keys

--- Project/tables.sql
+++ Project/tables.sql
@@ -1,14 +1,14 @@
-DROP TABLE supplier;
-DROP TABLE supplierPhone;
-DROP TABLE shipment;
-DROP TABLE supplies;
-DROP TABLE ships;
-DROP TABLE receives;
-DROP TABLE contains;
-DROP TABLE lot;
-DROP TABLE madeFrom;
-DROP TABLE productLine;
 DROP TABLE sells;
+DROP TABLE madeFrom;
+DROP TABLE contains;
+DROP TABLE receives;
+DROP TABLE ships;
+DROP TABLE shipment;
+DROP TABLE supplierPhone;
+DROP TABLE supplies;
+DROP TABLE lot;
+DROP TABLE supplier;
+DROP TABLE productLine;
 
 CREATE TABLE supplier (
     supplierId NUMBER PRIMARY KEY,
@@ -22,48 +22,11 @@ CREATE TABLE supplier (
 
 CREATE TABLE supplierPhone (
 	supplierId NUMBER,
-	phoneNumber VARCHAR(20)
-);
-
-CREATE TABLE shipment (
-    shipmentId NUMBER PRIMARY KEY,
-    unitPrice DECIMAL(10, 4) NOT NULL,
-    quantity NUMBER,
-    supplierId NUMBER
-);
-
-CREATE TABLE supplies (
-	supplierId NUMBER,
-	receiverId NUMBER,
-	CONSTRAINT supplies_pk PRIMARY KEY (supplierId, receiverId)
-);
-
-CREATE TABLE ships (
-	shipmentId NUMBER PRIMARY KEY,
-	supplierId NUMBER
-);
-
-CREATE TABLE receives (
-	shipmentId NUMBER PRIMARY KEY,
-	supplierId NUMBER
-);
-
-CREATE TABLE contains (
-	shipmentId NUMBER,
-	lotId NUMBER,
-	CONSTRAINT contains_pk PRIMARY KEY (shipmentId, lotId)
-);
-
-CREATE TABLE lot (
-	lotId NUMBER PRIMARY KEY,
-	supplierId NUMBER,
-	productLineId NUMBER
-);
-
-CREATE TABLE madeFrom (
-	productLineId NUMBER,
-	lotId NUMBER,
-	CONSTRAINT madeFrom_pk PRIMARY KEY (productLineId, lotId)
+	phoneNumber VARCHAR(20),
+	
+	FOREIGN KEY (supplierId)
+        REFERENCES supplier(supplierId)
+        ON DELETE CASCADE
 );
 
 CREATE TABLE productLine (
@@ -72,9 +35,114 @@ CREATE TABLE productLine (
 	price DECIMAL (10, 4)
 );
 
+CREATE TABLE lot (
+	lotId NUMBER PRIMARY KEY,
+	supplierId NUMBER,
+	productLineId NUMBER,
+	
+	FOREIGN KEY (supplierId)
+		REFERENCES supplier(supplierId)
+		ON DELETE SET NULL,
+	
+	FOREIGN KEY (productLineId)
+		REFERENCES productLine(productLineId)
+		ON DELETE SET NULL
+);
+
+CREATE TABLE shipment (
+    shipmentId NUMBER PRIMARY KEY,
+    unitPrice DECIMAL(10, 4) NOT NULL,
+    quantity NUMBER,
+    supplierId NUMBER,
+	
+	FOREIGN KEY (supplierId)
+        REFERENCES supplier(supplierId)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE supplies (
+	supplierId NUMBER,
+	receiverId NUMBER,
+	
+	CONSTRAINT supplies_pk PRIMARY KEY (supplierId, receiverId),
+	
+	FOREIGN KEY (supplierId)
+        REFERENCES supplier(supplierId)
+        ON DELETE SET NULL,
+		
+	FOREIGN KEY (receiverId)
+        REFERENCES supplier(supplierId)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE ships (
+	shipmentId NUMBER PRIMARY KEY,
+	supplierId NUMBER,
+	
+	FOREIGN KEY (shipmentId)
+        REFERENCES shipment(shipmentId)
+        ON DELETE SET NULL,
+		
+	FOREIGN KEY (supplierId)
+        REFERENCES supplier(supplierId)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE receives (
+	shipmentId NUMBER PRIMARY KEY,
+	supplierId NUMBER,
+	
+	FOREIGN KEY (shipmentId)
+        REFERENCES shipment(shipmentId)
+        ON DELETE SET NULL,
+		
+	FOREIGN KEY (supplierId)
+        REFERENCES supplier(supplierId)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE contains (
+	shipmentId NUMBER,
+	lotId NUMBER,
+	
+	CONSTRAINT contains_pk PRIMARY KEY (shipmentId, lotId),
+
+	FOREIGN KEY (lotId)
+        REFERENCES lot(lotId)
+        ON DELETE SET NULL,
+		
+	FOREIGN KEY (shipmentId)
+        REFERENCES shipment(shipmentId)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE madeFrom (
+	productLineId NUMBER,
+	lotId NUMBER,
+	
+	CONSTRAINT madeFrom_pk PRIMARY KEY (productLineId, lotId),
+	
+	FOREIGN KEY (productLineId)
+        REFERENCES productLine(productLineId)
+        ON DELETE SET NULL,
+		
+	FOREIGN KEY (lotId)
+        REFERENCES lot(lotId)
+        ON DELETE SET NULL
+);
+
 CREATE TABLE sells (
 	supplierId NUMBER,
 	productLineId NUMBER,
 	salePrice DECIMAL (10, 4),
-	CONSTRAINT sells_pk PRIMARY KEY (supplierId, productLineId)
+	
+	CONSTRAINT sells_pk PRIMARY KEY (supplierId, productLineId),
+
+	FOREIGN KEY (supplierId)
+        REFERENCES supplier(supplierId)
+        ON DELETE SET NULL,
+	
+	FOREIGN KEY (productLineId)
+        REFERENCES productLine(productLineId)
+        ON DELETE SET NULL
 );
